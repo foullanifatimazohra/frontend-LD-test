120651885d5745fb6c91c222acf344e8733caa20
foullanifatimazohra <foullanifatoum@gmail.com>
2024-01-09 19:40:29 +0100

    feat : creare No data component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import TextField from "./components/text-field/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
+import { NoData } from "./components/table/NoData";
 
 function App() {
   const [searchValue, setSearchValue] = useState<string>("");
@@ -13,6 +14,7 @@ function App() {
         value={searchValue}
         setValue={setSearchValue}
       />
+      <NoData />
     </>
   );
 }
